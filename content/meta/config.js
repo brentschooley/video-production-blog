04cfaa5088b8b2cae14b5f6a484756e1245c33d5
brentschooley <schooleybrent@gmail.com>
2019-03-26 18:45:27 -0400

    Update config.js

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  siteTitle: "StartStreaming.dev", // <title>
+  shortSiteTitle: "StartStreaming.dev - Guides for Livestreaming Code", // <title> ending for posts and pages
+  siteDescription: "Guides for Livestreaming Code",
+  siteUrl: "https://startstreaming.dev",
   // pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
 
   /* author */
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "brent schooley",
+  authorTwitterAccount: "brentschooley",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "StartStreaming.dev",
+  headerSubTitle: "guides for livestreaming code",
 
   /* manifest.json */
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "StartStreaming.dev - Guides for Livestreaming Code",
+  manifestShortName: "StreamGuides", // max 12 characters
   manifestStartUrl: "/index.html",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
@@ -28,12 +28,12 @@ module.exports = {
   // Replace your email adress with md5-code.
   // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
   // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
-  gravatarImgMd5: "",
+  gravatarImgMd5: "https://www.gravatar.com/avatar/99af192e40955c3c0cef601847b6650b",
 
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/brentschooley" },
+    { name: "twitter", url: "https://twitter.com/brentschooley" },
+    { name: "facebook", url: "http://facebook.com/brentschooley" }
   ]
 };
